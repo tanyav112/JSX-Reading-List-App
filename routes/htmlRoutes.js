c29de1af7e8a6a1eee9a8fb489ef444f12c5096c
Tanya Valdez <TValdez@digmap.com>
2018-10-30 22:19:24 -0700

    add htmlroutes.js

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,12 @@
+// const books = require('../data/readingList')
+
+// module.exports = (app) => {
+//   app.get('/', (req, res) => res.render('index'))
+//   // app.get('/books', (req, res) => {res.render('books')})
+
+//   // app.get('/bookView', (req, res) => {res.render('bookView', {books: books.getBooks()})})
+
+//   // app.post('/books', (req, res) => {
+//   //   books.postBooks(req.body)
+//   // })
+  // 
