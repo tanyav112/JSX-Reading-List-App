b3394d9b537a2a183bc9350d7ed291b370bc6abd
Tanya Valdez <TValdez@digmap.com>
2018-11-03 07:57:13 -0700

    Removed form route because it was added as component

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ module.exports = app => {
 
   app.get("/", (req, res) => res.render("index"))
   app.get("/books", (req, res) => res.render("books"));
-  app.get("/forms", (req, res) => res.render("form"))
+
 
 }
